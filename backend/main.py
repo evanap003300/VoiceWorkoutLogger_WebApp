4e2ef0b12af83ac138377552f7e117b1a9311910
Evan Phillips <evanap003300@gmail.com>
2025-07-22 20:34:32 -0400

    Adding files

--- backend/main.py
+++ backend/main.py
@@ -10,12 +10,14 @@ import os
 
 app = FastAPI()
 
+# allow_origins=[
+#        "https://voice-workout-logger-web-app.vercel.app",
+#        "https://voiceworkoutlogger-webapp.onrender.com"
+#    ], 
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://voice-workout-logger-web-app.vercel.app",
-        "https://voiceworkoutlogger-webapp.onrender.com"
-    ],  
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
